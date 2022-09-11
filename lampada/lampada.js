4387f76024a8baad0f9c7fbc9e0428f9f22625c4
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2022-09-10 21:32:23 -0300

    att

---
+++ lampada/lampada.js
@@ -0,0 +1,15 @@
+function entrou(){
+  document.getElementById('imglamp').src="imagens/ligada.jpg" 
+}
+function saiu(){
+  document.getElementById('imglamp').src="imagens/desligada.jpg" 
+}
+function ligar(){
+  document.getElementById('imglamp').src="imagens/ligada.jpg" 
+}
+function desligar(){
+  document.getElementById('imglamp').src="imagens/desligada.jpg" 
+}
+function click2x(){
+  document.getElementById('imglamp').src="imagens/quebrada.jpg" 
+}
