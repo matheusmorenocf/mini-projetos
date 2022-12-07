7a41f02a00acc74988b9d0ad316177cecc3206d8
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2022-12-06 22:07:30 -0300

    Update index.js

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -2,22 +2,11 @@ import { calculate, keyClick, keyPress, clear } from "./calculate.js"
 import { copy } from "./copy.js"
 import { themeSwitcher } from "./themeSwitcher.js"
 
-
-const input = document.getElementById('input')
-
-input.focus()
-
-document.querySelector('body').addEventListener('click', () => input.focus())
-
-input.addEventListener('keydown', keyPress )
-
+document.querySelector('body').addEventListener('click', () => document.getElementById('input').focus())
+document.getElementById('input').addEventListener('keydown', keyPress )
 document.querySelectorAll('.charKey').forEach( keyClick )
-
 document.getElementById('clear').addEventListener('click', clear)
-
 document.getElementById('equal').addEventListener('click', calculate)
-
 document.getElementById('copyToClipboard').addEventListener('click', copy)
-
 document.getElementById('themeSwitcher').addEventListener('click', themeSwitcher)
 
