08b908630eedaad90fec2cfefee2d2a89bda3549
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-02-18 10:36:49 -0300

    Update

--- lamp/lampada.js
+++
@@ -1,9 +0,0 @@
-function ligar(){
-  document.getElementById('imglamp').src="imagens/ligada.jpg" 
-}
-function desligar(){
-  document.getElementById('imglamp').src="imagens/desligada.jpg" 
-}
-function click2x(){
-  document.getElementById('imglamp').src="imagens/quebrada.jpg" 
-}
